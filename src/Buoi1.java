37376550cd01697fdeee6795a474da6073285a64
Phat <phatdroid94@gmail.com>
2022-04-02 19:30:31 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,30 @@
+public class Buoi1 {
+
+    // method , function ==  Phương thức
+    public static void main(String[] args) {
+
+//         1 Kiểu dữ liệu
+//
+//         Có 2 nhóm
+//         Kiểu nguyên thủy (primitives) : Không cung cấp chức năng để xử lý dữ liệu
+//             String (chuỗi) => "Nguyễn Văn tèo"
+//             char (ký tự) => 'a'
+//             int (số nguyên) , double lớn hơn int
+//             float (số thực) ,
+//             boolean(Kiểu đúng hoặc sai)
+//         Kiểu đối tượng (object) : Các phương thức hỗ trợ xử lý dữ liệu
+//             Tạo ra một đối tượng Person có thuộc tính name , age
+//
+//         2 Biến
+        String name = "Phạm Tấn Phát";
+        int age = 28;
+        float height = 1.75f;
+        String favorite = "Basketball";
+        double phone = 0352408366d;
+        String address = "Lý Thường Kiệt , Quận 10";
+        boolean isMale = false;
+
+
+
+    }
+}
