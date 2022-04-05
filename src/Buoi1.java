6f5fb71a9429ea3ed67eb7052cbcfe81d7f6688b
Phat <phatdroid94@gmail.com>
2022-04-05 18:42:26 +0700

    toan tu

--- src/Buoi1.java
+++ src/Buoi1.java
@@ -16,15 +16,51 @@ public class Buoi1 {
 //             Tạo ra một đối tượng Person có thuộc tính name , age
 //
 //         2 Biến
-        String name = "Phạm Tấn Phát";
-        int age = 28;
-        float height = 1.75f;
-        String favorite = "Basketball";
-        double phone = 0352408366d;
-        String address = "Lý Thường Kiệt , Quận 10";
-        boolean isMale = false;
+//        String name = "Phạm Tấn Phát";
+//        int age = 28;
+//        float height = 1.75f;
+//        String favorite = "Basketball";
+//        double phone = 0352408366d;
+//        String address = "Lý Thường Kiệt , Quận 10";
+//        boolean isMale = false;
 
+//        3 Toán tử
+//        int a = 5;
+//        a = a + 1;
+//        a += 1;
+//        int b = a++;
+        // Nếu dùng a++
+        // Biểu thức 1 : lấy giá trị a gán cho thằng bên tay trái
+        // Biểu thức 2 : tăng a lên 1 đơn vị
+        // Nếu dùng ++a thì ngược lại
+        // Phương thức in ra trong hệ thống
+        // ctrl + p : xem tham số truyền vào
+//        System.out.println(b);
 
+        int a = 5;
+        int b = 6;
+        int result = a++ - b-- + --a + b-- - --b + a-- - b--;
+        // 5 - b-- + --a + b-- - --b + a-- - b--; a = 6 , b = 6
+        // 5 - 6 + --a + b-- - --b + a-- - b--; a = 6 , b = 5
+        // 5 - 6 + 5 + b-- - --b + a-- - b--; a = 5 , b = 5
+        // 5 - 6 + 5 + 5 - --b + a-- - b--; a = 5 , b = 4
+        // 5 - 6 + 5 + 5 - 3 + a-- - b--; a = 5 , b = 3
+        // 5 - 6 + 5 + 5 - 3 + 5 - b--; a = 4 , b = 3
+        // 5 - 6 + 5 + 5 - 3 + 5 - 3 ; a = 4 , b = 2
+        // result = 8 ,a = 4 , b = 2
+
+        // in
+        // a : ketqua cua a
+        // b : ketqua cua b
+        // result : ketqua cua result
+
+        // Xử lý sâu chuỗi
+        // Toán tử + nếu sử dụng cho chuỗi nó sẽ mang ý nghĩa  là phép nối chuỗi
+//        int age = 20;
+//         "Age : " + age => Chuỗi
+        System.out.println("A : " + a);
+        System.out.println("B : " + b);
+        System.out.println("Result : " + result);
 
     }
 }
